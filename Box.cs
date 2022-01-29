154a57a478038de7db0112cc627230ee41fc8102
Zhu Panda <zhupandahugo@gmail.com>
2022-01-29 20:09:07 +0800

    add basic box drawing

---
+++ Box.cs
@@ -0,0 +1,58 @@
+using System;
+namespace ZP.CSharp.TerminalUI
+{
+    class Box
+    {
+        public int Height;
+        public int Width;
+        public int Left;
+        public int Top;
+        public string Title;
+        public Box(int height, int width, int left, int top, string title)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Left = left;
+            this.Top = top;
+            this.Title = title;
+        }
+        public void Draw()
+        {
+            Draw(this.Height, this.Width, this.Left, this.Top, this.Title);
+        }
+        public void Draw(int spacing)
+        {
+            Draw(this.Height, this.Width, this.Left, this.Top, this.Title, spacing);
+        }
+        public void Draw(int height, int width, int left, int top, string title, int spacing = 2)
+        {
+            if (spacing > (width - 2 - title.Length))
+            {
+                throw new InvalidOperationException("Spacing and/or title length exceeds width.");
+            }
+            Console.Write($"\x1b[{top + 1};{left + 1}H");
+            Console.Write(Border.TopLeft.MapToChar());            
+            for (int i = 0; i < (width - 2); i++)
+            {
+                if (i >= spacing && (i - spacing) < title.Length)
+                {
+                    Console.Write(title[i - spacing]);
+                }
+                else
+                {
+                    Console.Write(Border.Horizontal.MapToChar());
+                }
+            }
+            Console.Write($"{Border.TopRight.MapToChar()}\n");
+            for (int i = 0; i < (height - 2); i++)
+            {
+                Console.Write($"\x1b[{left + 1}G{Border.Vertical.MapToChar()}{new string(' ', width - 2)}{Border.Vertical.MapToChar()}");
+                if (i != (height - 2))
+                {
+                    Console.Write('\n');
+                }
+            };
+            Console.Write($"\x1b[{left + 1}G{Border.BottomLeft.MapToChar()}{new string(Border.Horizontal.MapToChar(), width - 2)}{Border.BottomRight.MapToChar()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+namespace ZP.CSharp.TerminalUI
+{
+    enum Border
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+        Vertical,
+        Horizontal
+    }
+    static class BorderExtensions
+    {
+        public static char MapToChar(this Border piece)
+        {
+            return piece switch
+            {
+                Border.TopLeft => '┌',
+                Border.TopRight => '┐',
+                Border.BottomLeft => '└',
+                Border.BottomRight => '┘',
+                Border.Horizontal => '─',
+                Border.Vertical => '│',
+                _ => '?'
+            };
+        }
+   }
+    class Window
+    {
+        public static void EnterBuffer()
+        {
+            Console.Write("\x1b[?1049h");
+            Console.Write("\x1b[1;1H");
+        }
+        public static void ExitBuffer()
+        {
+            Console.Write("\x1b[?1049l");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Window.EnterBuffer();
+            (int x, int y) cursor = Console.GetCursorPosition();
+            var b = new Box(20, 20, 0, 0, ".NET Core!");
+            b.Draw(9);
+            Thread.Sleep(5000);
+            Window.ExitBuffer();
+        }
+    }
+}
