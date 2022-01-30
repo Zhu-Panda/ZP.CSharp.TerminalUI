b4dcb177f15185ad618621bd88ed5b83ec0bb12d
Zhu Panda <zhupandahugo@gmail.com>
2022-01-30 18:27:19 +0800

    add more basic rendering support

--- VT100Seqs.cs
+++ VT100Seqs.cs
@@ -11,6 +11,14 @@ namespace ZP.CSharp.TerminalUI
         public static readonly string Blink = "\x1b[5m";
         public static readonly string Invert = "\x1b[7m";
         public static readonly string Hide = "\x1b[8m";
+        public static readonly string Strikethrough = "\x1b[9m";
+        public static readonly string NoBold = "\x1b[22m";
+        public static readonly string NoItalic = "\x1b[23m";
+        public static readonly string NoUnderline = "\x1b[24m";
+        public static readonly string NoBlink = "\x1b[25m";
+        public static readonly string NoInvert = "\x1b[27m";
+        public static readonly string Show = "\x1b[28m";
+        public static readonly string NoStrikethrough = "\x1b[29m";
     }
 }
 
