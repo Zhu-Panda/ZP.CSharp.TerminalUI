7ffd5307072e9ef5ea72b871d5f12b7b6985e9b6
Zhu Panda <zhupandahugo@gmail.com>
2022-02-01 12:14:30 +0800

    add style control for box

--- Box.cs
+++ Box.cs
@@ -1,4 +1,5 @@
 using System;
+using ZP.CSharp.TerminalUI;
 namespace ZP.CSharp.TerminalUI
 {
     class Box
@@ -20,17 +21,25 @@ namespace ZP.CSharp.TerminalUI
         {
             Draw(this.Height, this.Width, this.Left, this.Top, this.Title);
         }
-        public void Draw(int spacing)
+        public void Draw(int titleSpacing)
         {
-            Draw(this.Height, this.Width, this.Left, this.Top, this.Title, spacing);
+            Draw(this.Height, this.Width, this.Left, this.Top, this.Title, titleSpacing);
         }
-        public void Draw(int height, int width, int left, int top, string title, int titleSpacing = 2)
+        public void Draw(string boxStyle)
+        {
+            Draw(this.Height, this.Width, this.Left, this.Top, this.Title, 2, boxStyle);
+        }
+        public void Draw(int titleSpacing, string boxStyle)
+        {
+            Draw(this.Height, this.Width, this.Left, this.Top, this.Title, titleSpacing, boxStyle);
+        }
+        public void Draw(int height, int width, int left, int top, string title, int titleSpacing = 2, string boxStyle = ConsoleOutput.Default)
         {
             if (titleSpacing > (width - 2 - title.Length))
             {
                 throw new InvalidOperationException("Spacing and/or title length exceeds width.");
             }
-            Console.Write($"\x1b[{top + 1};{left + 1}H");
+            Console.Write($"\x1b[{top + 1};{left + 1}H{boxStyle}");
             Console.Write(Border.TopLeft.MapToChar());            
             for (int i = 0; i < (width - 2); i++)
             {
@@ -43,16 +52,16 @@ namespace ZP.CSharp.TerminalUI
                     Console.Write(Border.Horizontal.MapToChar());
                 }
             }
-            Console.Write($"{Border.TopRight.MapToChar()}\n");
+            Console.Write($"{Border.TopRight.MapToChar()}{ConsoleOutput.Default}\n");
             for (int i = 0; i < (height - 2); i++)
             {
-                Console.Write($"\x1b[{left + 1}G{Border.Vertical.MapToChar()}{new string(' ', width - 2)}{Border.Vertical.MapToChar()}");
+                Console.Write($"\x1b[{left + 1}G{boxStyle}{Border.Vertical.MapToChar()}{new string(' ', width - 2)}{Border.Vertical.MapToChar()}{ConsoleOutput.Default}");
                 if (i != (height - 2))
                 {
                     Console.Write('\n');
                 }
             };
-            Console.Write($"\x1b[{left + 1}G{Border.BottomLeft.MapToChar()}{new string(Border.Horizontal.MapToChar(), width - 2)}{Border.BottomRight.MapToChar()}");
+            Console.Write($"\x1b[{left + 1}G{boxStyle}{Border.BottomLeft.MapToChar()}{new string(Border.Horizontal.MapToChar(), width - 2)}{Border.BottomRight.MapToChar()}{ConsoleOutput.Default}");
         }
     }
 }
