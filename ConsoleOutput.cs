e65efa8dcca762dcf3f5725d04f6c2b164738c48
Zhu Panda <zhupandahugo@gmail.com>
2022-02-02 11:42:02 +0800

    remove extra newline

--- ConsoleOutput.cs
+++ ConsoleOutput.cs
@@ -81,8 +81,7 @@ namespace ZP.CSharp.TerminalUI
         public static string Hyperlink(string url, string desc, string id = "")
         {
             return $"\x1b]8;id={id};{url}\x1b\\{desc}\x1b]8;;\x1b\\";
-        }
-        
+        }        
     }
 }
 
